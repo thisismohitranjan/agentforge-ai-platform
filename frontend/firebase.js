b32000a8e51e4a93562f8f9997830b634c43aba4
hellojarvish61 <mohitranjan61@gmail.com>
2026-07-13 15:01:26 +0530

    initial commit

---
+++ frontend/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY ,
+  // authDomain: add your auth domain,
+  // projectId: add your project id,
+  // storageBucket: add your storage bucket,
+  // messagingSenderId: add your,
+  // appId: add your
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth=getAuth(app)
+export const googleProvider =
+  new GoogleAuthProvider();
+
+export const githubProvider =
+  new GithubAuthProvider();
